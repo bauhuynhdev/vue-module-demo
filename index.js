5ec78e7f725df1cf498f2ffd217bca4a249a811c
Bau Huynh <bauhuynhdev@gmail.com>
2018-10-03 19:47:30 +0700

    - 1.1.1

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-module.exports = function (number, locale) {
-  return number.toLocaleString(locale)
+export default {
+  install(Vue, options) {
+    Vue.mixin({
+      mounted() {
+        console.log('Vue-module-demo')
+      }
+    }),
+      Vue.component('vue-module-demo', {
+        template: '<h1>Vue module demo</h1>'
+      })
+  }
 }
